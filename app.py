4a5a4a10962ce8ede58cde8fc02863bd04672399
zeroxy <zeroxy@users.noreply.github.com>
2023-04-18 10:24:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,106 +10,91 @@ import json
 from datetime import date
 from joblib import Parallel, delayed, cpu_count
 
-@app.route('/')
-def hello_world():
-    resultstr = get_lucky_number()
-    return f"<html><body><pre>{resultstr}</pre></body></html>"
 
+def print_lotto_beautiful(games):
+	resultstr =""
+	result_game_no  = len(games)
+	printstr = [ "o"*45 for _ in range(result_game_no)]
+	for idx, x in enumerate(games):
+		for y in x:
+			printstr[idx] = printstr[idx][:y-1]+'X'+printstr[idx][y:]
+	column = 6
+	for row_no in range((45//column)+1):
+		row = [f"{x[row_no*column : row_no*column+column]:6}" for x in printstr]
+		resultstr = resultstr + "  |  ".join(row) + "\n"
+	return resultstr
+
+def softmax(x):
+	if np.sum(x < 0 ) >0:
+		y = x - np.min(x)
+	else :
+		y = x
+	return  y / np.sum(y)
+
+def get_lotto(no):
+	r= json.load(httpcl.urlopen(f"https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={no}"))
+	if r['returnValue'] != 'success':
+		return None,None
+	crawl =  [r[f'drwtNo{x+1}'] for x in range(6)]
+	crawl.sort()
+	rstr = f"{r['drwNo']} {r['drwNoDate']} {crawl}"
+	return_obj = {'no' : int(r['drwNo']), 'date_str':r['drwNoDate'], 'nums': crawl}
+	return return_obj
+
+def get_recent_game_no():
+	startdate = date(2002,12,7)
+	enddate = date.today()
+	dday = (enddate - startdate).days
+	lasttime = dday//7 +2
+	return lasttime
+
+def get_luck(choice_probs):
+		return np.sort( np.random.choice(45 , size=6, replace=False, p=choice_probs) +1)
+
+def get_lucky_nums(label, temp_choic_probs):
+	resultstr = "#####"*8+"\n"
+	
+	resultstr = resultstr + f"{label}  ( min : {np.min(temp_choic_probs):.3f} , max : {np.max(temp_choic_probs):.3f} , sum : { np.sum(temp_choic_probs):.3f})\n"
+	resultstr = resultstr + f"{temp_choic_probs}\n"
+
+	result_game_no = 100_000
+	result = np.zeros((result_game_no,6), dtype=np.int64)
+
+	for x in range(result_game_no):
+		result[x] = get_luck(temp_choic_probs)
+	temp =  np.sum((np.ones(1, dtype=np.int64)*2)**result, axis=1)
+	result_nums, r_idx, case_cnt = np.unique(temp, return_counts=True, return_index= True)
+
+	sorted_idx = np.argsort(case_cnt)[-5:]
+	resultstr = resultstr + f"duplicate count : {case_cnt[sorted_idx]}\n"
+	result = result[ r_idx[sorted_idx]]
+	
+	resultstr = resultstr + f"{result}\n"
+	resultstr = resultstr + print_lotto_beautiful(result)
+	resultstr = resultstr +"\n" +"#####"*8
+	return resultstr
+	
+
+
+
+
+def get_probs():
+	facts = [ x for x in range (46)]
+	print(facts)
+	for i in range(2,46):
+		facts[i] = facts[i] * facts[i-1]
+	print(facts)
+
+	C44_6 = int(facts[44] / (facts[38]*facts[6]) )
+	C44_5 = int(facts[44] / (facts[39]*facts[5]) )
+	C45_6 = int(facts[45] / (facts[39]*facts[6]) )
+
+	print(C44_6, C44_5,C44_6+ C44_5)
+	print(C45_6)
+	return C44_5 / C45_6
+
+	
 def get_lucky_number():
-    def print_lotto_beautiful(games):
-        resultstr =""
-        result_game_no  = len(games)
-        printstr = [ "o"*45 for _ in range(result_game_no)]
-        for idx, x in enumerate(games):
-            for y in x:
-                printstr[idx] = printstr[idx][:y-1]+'X'+printstr[idx][y:]
-        column = 6
-        for row_no in range((45//column)+1):
-            row = [f"{x[row_no*column : row_no*column+column]:6}" for x in printstr]
-            resultstr = resultstr + "  |  ".join(row) + "\n"
-        return resultstr
-
-    def softmax(x):
-        if np.sum(x < 0 ) >0:
-            y = x - np.min(x)
-        else :
-            y = x
-        return  y / np.sum(y)
-
-    def get_lotto(no):
-        r= json.load(httpcl.urlopen(f"https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={no}"))
-        if r['returnValue'] != 'success':
-            return None,None
-        crawl =  [r[f'drwtNo{x+1}'] for x in range(6)]
-        crawl.sort()
-        rstr = f"{r['drwNo']} {r['drwNoDate']} {crawl}"
-        return_obj = {'no' : int(r['drwNo']), 'date_str':r['drwNoDate'], 'nums': crawl}
-        return return_obj
-
-    def get_recent_game_no():
-        startdate = date(2002,12,7)
-        enddate = date.today()
-        dday = (enddate - startdate).days
-        lasttime = dday//7 +2
-        return lasttime
-    
-    def get_luck(choice_probs):
-            return np.sort( np.random.choice(45 , size=6, replace=False, p=choice_probs) +1)
-
-    def get_lucky_nums(label, temp_choic_probs):
-        resultstr = "#####"*8+"\n"
-        
-        resultstr = resultstr + f"{label}  ( min : {np.min(temp_choic_probs):.3f} , max : {np.max(temp_choic_probs):.3f} , sum : { np.sum(temp_choic_probs):.3f})\n"
-        resultstr = resultstr + f"{temp_choic_probs}\n"
-    
-        result_game_no = 1000
-        result = np.zeros((result_game_no,6), dtype=np.int64)
-
-        for x in range(result_game_no):
-            result[x] = get_luck(temp_choic_probs)
-        temp =  np.sum((np.ones(1, dtype=np.int64)*2)**result, axis=1)
-        result_nums, r_idx, case_cnt = np.unique(temp, return_counts=True, return_index= True)
-
-        sorted_idx = np.argsort(case_cnt)[-5:]
-        resultstr = resultstr + f"duplicate count : {case_cnt[sorted_idx]}\n"
-        result = result[ r_idx[sorted_idx]]
-        
-        resultstr = resultstr + f"{result}\n"
-        resultstr = resultstr + print_lotto_beautiful(result)
-        resultstr = resultstr +"\n" +"#####"*8
-        return resultstr
-        
-
-    lasttime = get_recent_game_no()
-    
-    starttime = 1
-    starttime = lasttime-10
-    
-    resultstr = ""
-    crawlNo=[]
-    cpucnt = cpu_count()
-    multipool = Parallel(n_jobs=(cpucnt*4))
-    poolfn = delayed(get_lotto)
-    crawlNo = multipool(poolfn(x) for x in range(starttime, lasttime) )
-    sorted(crawlNo, key=lambda x : x['no'])
-    resultstr = resultstr+ "\n".join([f"{x}" for x in crawlNo])+"\n"
-    
-    '''
-    def get_probs():
-        facts = [ x for x in range (46)]
-        print(facts)
-        for i in range(2,46):
-            facts[i] = facts[i] * facts[i-1]
-        print(facts)
-
-        C44_6 = int(facts[44] / (facts[38]*facts[6]) )
-        C44_5 = int(facts[44] / (facts[39]*facts[5]) )
-        C45_6 = int(facts[45] / (facts[39]*facts[6]) )
-
-        print(C44_6, C44_5,C44_6+ C44_5)
-        print(C45_6)
-        return C44_5 / C45_6
-    '''
     probs = 1086008 / 8145060  # 숫자 1이 포함될 확률
     sample_cnt = 20000000       # sample game 수
     game_contain_no_1 = np.random.rand(sample_cnt)<= probs  # sample game 수 만큼 수행 했을때 1이 포함된 게임을 True 외엔 False
@@ -145,12 +130,30 @@ def get_lucky_number():
     resultstr = resultstr+ get_lucky_nums("softmax_non_exp_choice_probs", softmax_non_exp_choice_probs) +"\n"
 
     print(resultstr)
-    return resultstr
+    return pre_resultstr+ resultstr
 
+
+global lasttime = get_recent_game_no()
+    
+global starttime = lasttime-10
+
+global pre_resultstr = ""
+global crawlNo=[]
+global cpucnt = cpu_count()
+global multipool = Parallel(n_jobs=(cpucnt*4))
+global poolfn = delayed(get_lotto)
+crawlNo = multipool(poolfn(x) for x in range(starttime, lasttime) )
+sorted(crawlNo, key=lambda x : x['no'])
+pre_resultstr = pre_resultstr+ "\n".join([f"{x}" for x in crawlNo])+"\n"
+print(pre_resultstr)
+
+	
+@app.route('/')
+def hello_world():
+    resultstr = get_lucky_number()
+    return f"<html><body><pre>{resultstr}</pre></body></html>"
+	
+	
 if __name__ == "__main__" :
-    #get_lucky_number()
-    app.run(host='0.0.0.0', port=8080)
-
-
-
-
+    get_lucky_number()
+    #app.run(host='0.0.0.0', port=8080)
